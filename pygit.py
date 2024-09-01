46dca0a079f2a389a99c95264a6fb6b932502e16
ShigrafS <shigrafsalik@proton.me>
2024-09-01 20:55:36 +0530

    Added the Hash object function to compute the hash of object data and return SHA! object hash as hex sstring

--- pygit.py
+++ pygit.py
@@ -6,7 +6,7 @@ def write_file(path, data):
         f.write(data)
 
 #Initializing Repository
-def innit(repo):
+def init(repo):
     #Cre4ating a directory for repository and initializing directory
     os.mkdir(repo)
     os.mkdir(os.path.join(repo, '.git'))
@@ -16,3 +16,15 @@ def innit(repo):
     write_file(os.path.join(repo,'.git','HEAD'), b'ref: refs/heads/main')
     
     print("Initialized empty repository: {}".format|(repo))
+
+def hash_object(data, obj_type, write=True):
+    #Compute the hash of the object data
+    header = '{}{}'.format(obj_type, len(data)).encode()
+    full_data = header + b'\x00' + data
+    sha1 = hashlib.sha1(full_data).hexdigest()
+    if write:
+        path = os.path.join('.git', 'objects', sha1[:2], sha1[2:])
+        if not os.path.exists(path):
+            os.makedirs(os.path.dirname(path), exist_ok=True)
+            write_file(path, zlib.compress(full_data))
+    return sha1
